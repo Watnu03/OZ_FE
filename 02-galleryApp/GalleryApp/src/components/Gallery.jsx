8eff47e2558fce416ec2ee843c7c7a3732376190
Watnu <watnu03@gmail.com>
2024-06-03 17:01:17 +0900

    6일차 GalleryApp

--- 02-galleryApp/GalleryApp/src/components/Gallery.jsx
+++ 02-galleryApp/GalleryApp/src/components/Gallery.jsx
@@ -1,15 +1,63 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 function Gallery(){
+  //변수 생성
+  const [keyword, setKeyword] = useState('');
+  const [list, setList] = useState([]);
+
+  const handleKeyword = (e) =>{
+    setKeyword(e.target.value);
+  }
+
+  //async: 함수앞에 붙여 일반함수를 async함수로 만들어 비동기 함수로 만든다
+  //함수의 앞부분에 async 키워드를 추가
+  const fetchPhotos = async (query) => {
+    /*Key같은경우 중요하기때문에 
+    1) .env.local파일을 생성해서 따로 key값 넣기 
+    2) .env.local파일은 개인정보이기 때문에 gitignore파일에서 예외처리하기
+    3) 회사에서는 똑같이 git에는 예외처리하고 팀원들에게는 따로 파일로 공유하기*/
+    const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${accessKey}`;
+  
+    /*에러가 발생 할 것 같은 코드를 try 로 감싸고 
+    에러가 발생했을 때 catch 를 이용하여 예외처리를 한다.*/
+    try {
+      //함수 내부에서 await 키워드를 사용
+      //fetch: API를 가져올때쓰는 객체
+      const response = await fetch(url);
+      const data = await response.json();
+      setList(data.results);
+    } 
+    catch (error) {
+        console.error('Error fetching photos:', error);
+        setList([]);
+    }
+  };
+
+  const handleSubmit = (e) =>{
+    //새로고침 방지
+    e.preventDefault();
+
+    if (keyword) fetchPhotos(keyword);
+  }
 
   return(
     <>
       <GalleryWrap>
-        <form>
-          <input type="text" placeholder='Search for images...' />
+        <form onSubmit={handleSubmit}>
+          <input type="text" 
+                placeholder='Search for images...'
+                value={keyword}
+                onChange={handleKeyword}  />
           <button>검색</button>
         </form>
         <ul>
+          {list.map(photo => (
+            <Photo key={photo.id} >
+              <img src={photo.urls.small} alt={photo.alt} />
+            </Photo>
+          ))}
         </ul>
       </GalleryWrap>
     </>
@@ -22,7 +70,16 @@ const GalleryWrap = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
+  overflow-y: scroll;
 
+  form{
+    width: 100%;
+    height: 60px;
+    text-align: center;
+    position: fixed;
+
+    background-color: #fff;
+  }
   input[type=text]{
     width: 150px;
     height: 30px;
@@ -49,12 +106,23 @@ const GalleryWrap = styled.div`
   button:hover{
     background-color: #5f5f5f;
   }
+  ul{
+  width: 1100px;
+  height: 100%;
+  margin-top: 50px;
+
+  display:flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  }
 `  
 const Photo = styled.div`
 img {
-    width: 100%;
-    height: auto;
-    border-radius: 8px;
+  width: 330px;
+  height: 300px;
+  object-fit: cover;
+  border-radius: 10px;
+  margin: 20px;
 }
 `
 
