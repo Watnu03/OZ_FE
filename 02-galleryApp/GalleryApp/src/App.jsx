434277099fb78d3f54b9311e4cb8e932454d6566
Watnu <watnu03@gmail.com>
2024-06-03 14:07:07 +0900

    6일차과제-갤러리앱 프로젝트 생성

---
+++ 02-galleryApp/GalleryApp/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+import './components/Gallery'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ 02-galleryApp/GalleryApp/src/components/Gallery.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import styled from "styled-components";
+
+function Gallery(){
+
+  return(
+    <>
+      <h1>hello wolrd</h1>
+    </>
+  )
+}
+
+export default Gallery
