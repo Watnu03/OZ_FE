0ce28811f485fec1b715103d88910a6f2f058867
Watnu <watnu03@gmail.com>
2024-06-03 14:45:22 +0900

    갤러리앱 html/css 작성

--- 02-galleryApp/GalleryApp/src/App.jsx
+++ 02-galleryApp/GalleryApp/src/App.jsx
@@ -1,11 +1,15 @@
-// import { useState } from 'react'
 import './App.css'
-import './components/Gallery'
+import Gallery from'./components/Gallery'
 
 function App() {
 
   return (
     <>
+      <main className='container'>
+        <h1>Image Gallery📷</h1>
+        <p>원하는 이미지를 검색하세요.</p>
+        <Gallery />
+      </main>
     </>
   )
 }
--- 02-galleryApp/GalleryApp/src/components/Gallery.jsx
+++ 02-galleryApp/GalleryApp/src/components/Gallery.jsx
@@ -1,13 +1,61 @@
-// import { useState } from "react";
 import styled from "styled-components";
 
 function Gallery(){
 
   return(
     <>
-      <h1>hello wolrd</h1>
+      <GalleryWrap>
+        <form>
+          <input type="text" placeholder='Search for images...' />
+          <button>검색</button>
+        </form>
+        <ul>
+        </ul>
+      </GalleryWrap>
     </>
   )
 }
+
+//styled-components 사용
+const GalleryWrap = styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  input[type=text]{
+    width: 150px;
+    height: 30px;
+    margin-right: 5px;
+    padding-left: 5px;
+
+    color: #7f8c8d;
+    outline: solid 0.1em #7f8c8d;
+    border-radius: 5px;
+  }
+  button{
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 00;
+    font-family: inherit;
+    cursor: pointer;
+    transition: border-color 0.25s;
+
+    background-color: #1A1A1A;
+    color: #f9f9f9;
+  }
+  button:hover{
+    background-color: #5f5f5f;
+  }
+`  
+const Photo = styled.div`
+img {
+    width: 100%;
+    height: auto;
+    border-radius: 8px;
+}
+`
 
 export default Gallery
