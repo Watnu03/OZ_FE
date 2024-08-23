1cc56c0049018ff7196c7af0776655ed3330a959
Watnu <107114225+Watnu03@users.noreply.github.com>
2024-08-23 14:02:05 +0900

    Add files via upload

---
+++ React/react-redux-app-main/src/App.js
@@ -0,0 +1,25 @@
+
+
+import './App.css';
+
+function App({value}) {
+ 
+  
+  
+  return (
+    <div className="App">
+      Clicked: {value} times
+      {" "}
+      <button>
+        +
+        </button>
+        {" "}
+        <button>
+          -
+          </button>
+          
+    </div>
+  );
+}
+
+export default App;
